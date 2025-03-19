5a8af20944c3abaa910e24cdc2be3984e4a06e74
Isaac Machado <isaacmachado.profissional@gmail.com>
2025-03-19 20:46:08 -0300

    Project Structure

---
+++ front-end/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/javascript/index.js', // Arquivo de entrada
+  output: {
+    filename: 'bundle.js', // Nome do bundle gerado
+    path: path.resolve(__dirname, 'dist'), // Caminho para a pasta dist
+    clean: true, // Limpa o diretório dist antes de cada build
+  },
+  module: {
+    rules: [
+      
+    {
+        test: /\.js$/, // Regras para arquivos JS
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',	
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    },
+
+    {
+        test: /\.css$/, 
+        use: [MiniCssExtractPlugin, 'css-Loader']
+    },
+
+      {
+        test: /\.html$/, // Processar arquivos HTML
+        use: ['html-loader'],
+      },
+    ],
+  },
+
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // Vai catar o index.html 
+      filename: 'index.html', // Nome gerado
+
+    }),
+
+    new MiniCssExtractPlugin({
+        filename: 'style.css',
+    }),
+
+  ],
+
+
+  devServer: { // Configurações do servidor local
+    static: {
+      directory: path.join(__dirname, 'dist'), // Diretório q vai catar pra upar p servidor
+    },
+    compress: true, // Ativar compressão gzip
+    port: 9000, // Porta 
+    open: true, // Abrir automaticamente o navgg
+  },
+  mode: 'development', 
+};
