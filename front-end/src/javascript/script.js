11cfe5d8c46f06474c91cfb157055d73e44f49c9
Isaac Machado <isaacmachado.profissional@gmail.com>
2025-04-06 12:19:31 -0300

    Feat: Mobile Hamburguer and improving continuous porting mobile version

--- front-end/src/javascript/script.js
+++ front-end/src/javascript/script.js
@@ -9,19 +9,19 @@ function showSection(section) {
             <ul >
                     <li>
                         <h3> Votação anual para o Representante do Parque </h3>
-                        <p3> Votação que ocorre anualmente para o próximo representante do parque, acompanhe em nossas redes! </p3>
+                        <p> Votação que ocorre anualmente para o próximo representante do parque, acompanhe em nossas redes! </p>
 
                     </li>
 
                     <li>
                         <h3> Reforma do Parque </h3>
-                        <p3> O Parque necessita de atenção quanto à sua construção, ampliação e manutenção.  </p3>
+                        <p> O Parque necessita de atenção quanto à sua construção, ampliação e manutenção.  </p>
 
                     </li>
                         
                     <li>
                         <h3> Constantes Reuniões  </h3>
-                        <p3> Inscreva-se em nossa Newsletter e receba os links para participação das reuniões do Parque! </p3>
+                        <p> Inscreva-se em nossa Newsletter e receba os links para participação das reuniões do Parque! </p>
                     </li>
                     
             </ul>       
@@ -31,12 +31,12 @@ function showSection(section) {
             <ul>
                 <li> 
                         <h3> Viva o Verde SP </h3>
-                        <p3> Evento ocorrido no ano de 2024 em parceria com a ONU, promovendo um Plano de Gestão Geral para os parques de São Paulo.  </p3>
+                        <p> Evento ocorrido no ano de 2024 em parceria com a ONU, promovendo um Plano de Gestão Geral para os parques de São Paulo.  </p>
                 </li>
 
                 <li>
                         <h3> Construção do Nosso Parque </h3>
-                        <p3> Atividade em colaboração com o C.E.U Alvarenga com o Fundamental 1, ensinando valores fundamentais de sustentabilidade à natureza. </p3>
+                        <p> Atividade em colaboração com o C.E.U Alvarenga com o Fundamental 1, ensinando valores fundamentais de sustentabilidade à natureza. </p>
                 </li>
                 
             </ul>
